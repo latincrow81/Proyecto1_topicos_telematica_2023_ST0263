921f0038e11fb5dc7a3c6eb4b8e23499f485f90f
Mauricio Escudero <mauricio.escudero@patientpop.com>
2023-03-14 23:07:35 -0500

    api gateway

--- api_gateway/app/services.py
+++ api_gateway/app/services.py
@@ -1,14 +1,19 @@
-import pika
+import os
+
+import grpc
+
+from api_gateway.protos.generated import files_pb2_grpc, files_pb2
+
+SERVER_ADDRESS = os.getenv("HOST_MOM")
+SERVER_PORT = os.getenv("PORT_MOM")
+
 
 def send_grpc_request():
-    pass
-
-
+    with grpc.insecure_channel(f'{SERVER_ADDRESS}:{SERVER_PORT}') as channel:
+        for file in files_pb2_grpc.MessagesStub(channel).GetSendMessage(files_pb2.SendMessageRequest()).files:
+            print(file.filename)
 
 
 def get_file_list_grpc():
     pass
 
-
-def get_file_list_rmq():
-    pass
