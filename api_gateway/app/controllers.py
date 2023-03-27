d33a4f9f8baea15c17893ed5235287645cbd8fd7
Mauricio Escudero <mauricio.escudero@patientpop.com>
2023-03-27 18:40:47 -0500

    wio

--- api_gateway/app/controllers.py
+++ api_gateway/app/controllers.py
@@ -4,7 +4,7 @@ import os
 import grpc
 from flask import Response
 
-from api_gateway.protos.generated import files_pb2_grpc
+from api_gateway.protos.generated import files_pb2_grpc, files_pb2
 
 host_grpc = os.getenv("HOST_GRPC")
 grpc_port = os.getenv("PORT_GRPC")
@@ -12,17 +12,21 @@ rmq_user = os.getenv('USER')
 rmq_password = os.getenv('PASSWORD')
 
 
-def get_file_list():
+def create_queue():
+    # TODO: abrir conexion grpc enviar mensaje que incluya nombre de cola la respuesta debe ser un mensaje "cola creada <nombre>"
     with grpc.insecure_channel(f'{host_grpc}:{grpc_port}') as channel:
-        # Cliente para el servicio de ListFiles
+        # Cliente para el servicio de Messages
         list_files_client = files_pb2_grpc.MessagesStub(channel)
-
-        # Se llama al servicio de ListFiles
-        response = list_files_client.GetSendMessage(files_pb2.ListFilesRequest())
-
-        # Se retorna la lista de archivos en formato JSON
+        # Se llama al servicio de send message
+        response = list_files_client.GetSendMessage(files_pb2.SendMessageRequest())
+        # Se retorna el resultado de la creacion
     return Response(status=200, response=json.dumps({"files": [file.filename for file in response.files]}))
 
+def post_to_queue():
+    # TODO: enviar mensaje con grpc igual que create queue
+    return Response(status=200, response={"exito": "Mensaje enviado a la cola"})
 
-def find_file():
-    return Response(status=404, response={"error": "Archivo no encontrado"})
+def read_from_queue():
+    # TODO: leer mensaje de cola con grpc
+    # TODO: response es el mensaje que retorne la cola
+    return Response(status=200, response={})
