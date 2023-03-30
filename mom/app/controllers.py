dc60e96f53d7888f3c9337b8d36f07f4d62ac911
Jose Sanchez <alejo3261@hotmail.com>
2023-03-29 21:32:36 -0500

    Cambios en memoria compartida

--- mom/app/controllers.py
+++ mom/app/controllers.py
@@ -1,5 +1,5 @@
 import os
-import multiprocessing
+from multiprocessing.shared_memory import SharedMemory, ShareableList
 
 host_grpc = os.getenv("HOST_GRPC")
 grpc_port = os.getenv("PORT_GRPC")
@@ -7,23 +7,20 @@ rmq_user = os.getenv('USER')
 rmq_password = os.getenv('PASSWORD')
 
 
-def create_queue(queue_name):
-    shm = multiprocessing.shared_memory.SharedMemory(create=True, size=2048) 
-    lista_queue_stack = shm.buf
-    lista_queue_stack[0] = 0 
-    return shm.name
+def create_queue(queue_name):     
+    shared_memory_list = ShareableList(name=queue_name)
+    return shared_memory_list
 
 def push_message_to_queue(queue_name, payload):
-    shm = multiprocessing.shared_memory.SharedMemory(name=queue_name)
-    lista_queue_stack = shm.buf
-    lista_queue_stack[lista_queue_stack[0] + 1] = payload
-    lista_queue_stack[0] += 1
-    shm.close()
+    temp_list = ShareableList(name=queue_name)
+    temp_list.append(payload)    
+    shared_memory_list = ShareableList(sequence=temp_list, name=queue_name)
+    return shared_memory_list
 
 def pop_message_from_queue(queue_name):
-    shm = multiprocessing.shared_memory.SharedMemory(name=queue_name)
-    lista_queue_stack = shm.buf
+    shared_memory_message = SharedMemory(name=queue_name)
+    lista_queue_stack = shared_memory_message.buf
     response = lista_queue_stack[lista_queue_stack[0]]
     lista_queue_stack[0] -= 1
-    shm.close()
+    shared_memory_message.close()
     return response
