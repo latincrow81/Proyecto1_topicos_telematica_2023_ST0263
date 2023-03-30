23841c6564fa8976d3f97323da9bb09c5477e162
Jose Sanchez <alejo3261@hotmail.com>
2023-03-29 20:23:35 -0500

    Memoria compartida

--- mom/app/controllers.py
+++ mom/app/controllers.py
@@ -1,4 +1,5 @@
 import os
+import multiprocessing
 
 host_grpc = os.getenv("HOST_GRPC")
 grpc_port = os.getenv("PORT_GRPC")
@@ -7,19 +8,22 @@ rmq_password = os.getenv('PASSWORD')
 
 
 def create_queue(queue_name):
-    # todo: crear espacio en memoria comparida con queue_name
-    return None
+    shm = multiprocessing.shared_memory.SharedMemory(create=True, size=10)
+    lista_queue_stack = shm.buf
+    lista_queue_stack[0] = 0 
+    return shm.name
 
 def push_message_to_queue(queue_name, payload):
-    lista_queue_stack = list()
-    lista_queue_stack.append(payload)
-    # meter en memoria lista_queue_stack compartida
-    # todo: push en stack payload en memoria compartida
-    return
+    shm = multiprocessing.shared_memory.SharedMemory(name=queue_name)
+    lista_queue_stack = shm.buf
+    lista_queue_stack[lista_queue_stack[0] + 1] = payload
+    lista_queue_stack[0] += 1
+    shm.close()
 
 def pop_message_from_queue(queue_name):
-    # todo: pop en stack de memoria compartida
-    lista_queue_stack = list()
-    response = lista_queue_stack.pop()
-
+    shm = multiprocessing.shared_memory.SharedMemory(name=queue_name)
+    lista_queue_stack = shm.buf
+    response = lista_queue_stack[lista_queue_stack[0]]
+    lista_queue_stack[0] -= 1
+    shm.close()
     return response
