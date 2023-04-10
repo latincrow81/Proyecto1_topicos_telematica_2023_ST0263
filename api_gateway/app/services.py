79c0b98605312b3e9de1ec52aa27bfccd636dbf0
Jose Sanchez <alejo3261@hotmail.com>
2023-04-10 11:35:02 -0500

    Cambios en rama al mom

--- api_gateway/app/services.py
+++ api_gateway/app/services.py
@@ -6,14 +6,16 @@ from api_gateway.app.protos import messages_pb2_grpc, messages_pb2
 
 config = dotenv_values("../.env")
 
-HOST_GRPC = config.get('HOST_GRPC')
-PORT_GRPC = config.get('PORT_GRPC')
+HOST = config.get('HOST')
+PORT = config.get('PORT')
 
 
 def send_message(message: messages_pb2.Message) -> str:
-    with grpc.insecure_channel(f"{HOST_GRPC}:{PORT_GRPC}") as channel:
+    with grpc.insecure_channel(f"{HOST}:{PORT}") as channel:
         stub = messages_pb2_grpc.MessagesStub(channel)
-        response = stub.GetSendMessage(message)
+        message_pb = messages_pb2.Message(queue_name=message.queue_name, op=message.op, payload=message.payload)
+        response = stub.GetSendMessage(message_pb)
     return response.result
+
 
 
--- mom/app/services.py
+++ mom/app/services.py
@@ -3,51 +3,49 @@ import grpc
 from concurrent import futures
 from enum import Enum
 from typing import Optional, Dict
-
 from app.controllers import create_queue
 from app.protos import messages_pb2_grpc, messages_pb2
 from dotenv import load_dotenv
 
 load_dotenv()
 
-SERVER_ADDRESS = os.getenv('HOST_MOM')
-GRPC_PORT = os.getenv('PORT_MOM')
+HOST = os.getenv('HOST')
+PORT = os.getenv('PORT')
 
 
-class ListFilesServicer(messages_pb2_grpc.MessagesServicer):
+class MessagesServicer(messages_pb2_grpc.MessagesServicer):
 
-    # Abre conexión
     def handle_grpc_request_from_gateway(self, request, context):
-
-        with grpc.insecure_channel(f"{SERVER_ADDRESS}:{GRPC_PORT}") as channel:
-
+        with grpc.insecure_channel(f"{HOST}:{PORT}") as channel:
             message = self.handle_request(request)
             return message
 
     #Recibe
     def GetSendMessage(self, request, context):
         message = self.handle_request(request)
-        return messages_pb2.Result(result=message)
+        result = messages_pb2.Result(result=message)
+        return result
 
     @staticmethod
     def handle_request(request) -> Optional[Dict]:
-        if request['op'] is Ops.CREATE:
-            queue_name = request.get('queue_name')
+        op = request.op.value
+        if op == Ops.CREATE.value:
+            queue_name = getattr(request, 'queue_name', None)
             create_queue(queue_name)
-        elif request['op'] is Ops.POST:
+        elif op == Ops.POST.value:
             pass
-        elif request['op'] is Ops.GET:
+        elif op == Ops.GET.value:
             pass
         else:
             pass
-
+        return{}
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    messages_pb2_grpc.add_MessagesServicer_to_server(messages_pb2_grpc.MessagesServicer(), server)
-    server.add_insecure_port('localhost:50051')
+    messages_pb2_grpc.add_MessagesServicer_to_server(MessagesServicer(), server)
+    server.add_insecure_port(f"{HOST}:{PORT}")
     server.start()
-    print(f'Servidor en ejecución en el puerto {GRPC_PORT}...')
+    print(f'Servidor en ejecución en el puerto {PORT}...')
     server.wait_for_termination()
 
 
