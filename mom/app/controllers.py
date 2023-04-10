bcf926590343b6bd3724affe7bbc6f4b70724a2e
Alejandro Torres <edialeto@hotmail.com>
2023-04-10 13:27:06 -0500

    add delete queue and topics

--- mom/app/controllers.py
+++ mom/app/controllers.py
@@ -8,11 +8,20 @@ rmq_user = os.getenv('USER')
 rmq_password = os.getenv('PASSWORD')
 
 
-# Controllador para operaciones de cola, como mvp todas las colas son de profundidad 5 y ordenamiento FIFO
+# Controllador para operaciones de cola y topico, como mvp todas las colas y topicos son de profundidad 5 y ordenamiento FIFO
 
-def create_queue(queue_name):     
+def create_queue(queue_name):
     shared_memory_list = ShareableList([' ' * 1024, ' ' * 1024, ' ' * 1024, ' ' * 1024, ' ' * 1024], name=queue_name)
     return shared_memory_list
+
+
+def delete_queue(queue_name):
+    try:
+        shared_memory_list = ShareableList(name=queue_name)
+        shared_memory_list.unlink()
+        return True
+    except FileNotFoundError:
+        return False
 
 
 def push_message_to_queue(queue_name, payload):
@@ -28,6 +37,42 @@ def push_message_to_queue(queue_name, payload):
 
 def pop_message_from_queue(queue_name):
     temp_list = ShareableList(name=queue_name)
+    for j in range(5):
+        if temp_list[j] == ' ' * 1024:
+            continue
+        else:
+            value = temp_list[j]
+            temp_list[j] = ' ' * 1024
+            return value
+
+
+def create_topic(topic_name):
+    shared_memory_list = ShareableList([' ' * 1024, ' ' * 1024, ' ' * 1024, ' ' * 1024, ' ' * 1024], name=topic_name)
+    return shared_memory_list
+
+
+def delete_topic(topic_name):
+    try:
+        shared_memory_list = ShareableList(name=topic_name)
+        shared_memory_list.unlink()
+        return True
+    except FileNotFoundError:
+        return False
+
+
+def push_message_to_topic(topic_name, payload):
+    temp_list = ShareableList(name=topic_name)
+    for j in range(5):
+        if temp_list[j] == ' ' * 1024:
+            temp_list[j] = json.dumps(payload)
+            return temp_list
+        else:
+            continue
+    return temp_list
+
+
+def pop_message_from_topic(topic_name):
+    temp_list = ShareableList(name=topic_name)
     for j in range(5):
         if temp_list[j] == ' ' * 1024:
             continue
